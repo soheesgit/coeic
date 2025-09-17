2b5490869b3a3673e7018c8e10ba66da31fbcca6
moonsohe <thgml89567@naver.com>
2025-09-18 00:03:35 +0900

    2차 교육 1일차 - 고급 Simulation (Q1 ~ Q4)

---
+++ src/main/java/org/example/week3_1/Q2.java
@@ -0,0 +1,53 @@
+package org.example.week3_1;
+
+
+import java.util.*;
+import java.io.*;
+
+public class Q2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[] top = new int[n];
+        int[] bottom = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            top[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            bottom[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < t; i++) {
+            int tempTop = top[n - 1]; //맨위벨트의 마지막값
+            int tempBottom = bottom[n - 1]; //맨아래벨트의 첫값
+
+            // 위 벨트: 오른쪽으로 한 칸씩 이동
+            for (int j = n - 1; j > 0; j--) {
+                top[j] = top[j - 1];
+            }
+
+            // 아래 벨트: 왼쪽으로 한 칸씩 이동
+            for (int j = n - 1; j > 0; j--) {
+                bottom[j] = bottom[j-1];
+            }
+
+            top[0] = tempBottom;
+            bottom[0] = tempTop;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(top[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(bottom[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/org/example/week3_1/Q4.java
@@ -0,0 +1,54 @@
+package org.example.week3_1;
+
+import java.util.*;
+import java.io.*;
+
+public class Q4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[][] grid = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] directionX = {-1, 1, 0, 0};  // 상, 하, 좌, 우
+        int[] directionY = {0, 0, -1, 1};
+        StringBuilder sb = new StringBuilder();
+
+        int nowX = r;
+        int nowY = c;
+
+        sb.append(grid[nowX][nowY]).append(" ");
+
+        while (true) {
+            boolean moved = false;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = nowX + directionX[i];
+                int ny = nowY + directionY[i];
+
+                if (nx < 1 || nx > n || ny < 1 || ny > n) continue;
+
+                if (grid[nowX][nowY] < grid[nx][ny]) {
+                    nowX = nx;
+                    nowY = ny;
+                    sb.append(grid[nx][ny]).append(" ");
+                    moved = true;
+                    break;
+                }
+            }
+
+            if (!moved) break;
+        }
+
+        System.out.print(sb.toString().trim());
+    }
+}
