5a5f7063b59a3f9f74dbf1f7625c98dce52eb714
moonsohe <thgml89567@naver.com>
2025-10-06 23:59:41 +0900

    2차 교육 5일차 과제 - HashSet, HashMap(Q7)

---
+++ src/main/java/org/example/week3_5/Q7.java
@@ -0,0 +1,56 @@
+package org.example.week3_5;
+
+import java.util.*;
+import java.io.*;
+
+public class Q7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] a = new int[k];
+        int[] b = new int[k];
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i] = Integer.parseInt(st.nextToken());
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        HashSet<Integer>[] visited = new HashSet[n + 1];
+        int[] arr = new int[n + 10];
+        int[] cnt = new int[n + 10];
+        for (int i = 1; i <= n; i++) { // 처음에 자리에 사람 채우기
+            visited[i] = new HashSet<Integer>();
+            arr[i] = i;
+            visited[i].add(i);
+            cnt[i] = 1;
+        }
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < k; i++) {
+
+                // 변환 로직
+                int temp = arr[a[i]];
+                arr[a[i]] = arr[b[i]];
+                arr[b[i]] = temp;
+
+                if (!visited[arr[a[i]]].contains(a[i])) {
+                    visited[arr[a[i]]].add(a[i]);
+                    cnt[arr[a[i]]]++;
+                }
+                if (!visited[arr[b[i]]].contains(b[i])) {
+                    visited[arr[b[i]]].add(b[i]);
+                    cnt[arr[b[i]]]++;
+                }
+
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(cnt[i]);
+        }
+
+
+    }
+}
