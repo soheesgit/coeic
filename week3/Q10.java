e575b90e77f9444811449999002213d73ee84bf9
moonsohe <thgml89567@naver.com>
2025-10-10 01:01:00 +0900

    feat: 2차 교육 5일차 과제 - HashSet, HashMap(Q10)

---
+++ week3/Q10.java
@@ -0,0 +1,54 @@
+package org.example.week2_5;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Q10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[n];
+        int[] B = new int[n];
+        int[] C = new int[n];
+        int[] D = new int[n];
+
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                int num = A[j] + B[i];
+                hash.put(num, hash.getOrDefault(num, 0) + 1); // 빈도수 저장
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            C[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            D[i] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                int target = - (C[j] + D[i]);
+
+                if (hash.containsKey(target)) cnt += hash.get(target);
+            }
+        }
+
+        System.out.println(cnt);
+
+
+    }
+}
