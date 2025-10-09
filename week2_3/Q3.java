f51f77c7053b320a9956cb9f77253ff5ab084f4b
moonsohe <thgml89567@naver.com>
2025-10-10 01:13:54 +0900

    2025 코익 집중교육 2차 - 3일차 : DP 1

---
+++ week2_3/Q3.java
@@ -0,0 +1,32 @@
+package org.example.week2_3;
+
+import java.util.*;
+import java.io.*;
+
+public class Q3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1; //모든 길이??1
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int min = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.max(min, dp[i]);
+        }
+        System.out.println(min);
+    }
+}
